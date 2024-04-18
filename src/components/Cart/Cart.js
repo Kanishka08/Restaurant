a96a5b02c0f6984c8a5b7617d41f754d648c18c7
Kanishka08 <kanishkachoudhury08@gmail.com>
2024-04-19 03:43:55 +0530

    Implementing Cart Items Page

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,12 +8,12 @@ const Cart = (props) => {
   return (
     <Modal onClose={props.onClose}>
         {cartItems}
-        <div>
-            <span>Total Amount</span>
-            <span>35.62</span>
+        <div className={classes.total}>
+            <span >Total Amount</span>
+            <span >35.62</span>
         </div>
         <div className={classes.actions}>
-            <button className={classes['button--alt']}> onClick={props.onClose}Close</button>
+            <button className={classes['button--alt']} onClick={props.onClose}> Close</button>
             <button className={classes.button}>Order</button>
         </div>
     </Modal>
